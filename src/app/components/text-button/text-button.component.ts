09f3b31c7819e8fd05cde34ab043ec14f9f2b6c6
Atlinx <atlinuxtm@gmail.com>
2021-11-22 17:40:05 -0500

    Add inheritance for buttons & close burger menu when navigation is chosen

--- src/app/components/text-button/text-button.component.ts
+++ src/app/components/text-button/text-button.component.ts
@@ -1,5 +1,6 @@
-import { Attribute, Component, Input, OnInit, Optional } from '@angular/core';
+import { Attribute, Component, Input, OnInit, Optional, Output, EventEmitter } from '@angular/core';
 import { Router } from '@angular/router';
+import { BaseButtonComponent } from '@app/utils/basebutton';
 import { Color } from '@app/utils/color';
 
 @Component({
@@ -7,29 +8,6 @@ import { Color } from '@app/utils/color';
   templateUrl: './text-button.component.html',
   styleUrls: ['./text-button.component.css']
 })
-export class TextButtonComponent implements OnInit {
+export class TextButtonComponent extends BaseButtonComponent {
 
-  @Input() width: number = -1;
-  @Input() color: string = "primary";
-  @Input() iconifyIcon: string = "ant-design:mail-filled";
-  @Input() link: string = "";
-  @Input() target: string = "_self";
-  
-  route: boolean = false;
-
-  constructor(
-    private router: Router,
-    @Optional() @Attribute('route') route: any) {
-    this.route = route != undefined;
-  }
-
-  ngOnInit(): void {
-  }
-
-  onClick(): void {
-    if (this.route)
-      this.router.navigateByUrl(this.link);
-    else
-      window.open(this.link, this.target);
-  }
 }
