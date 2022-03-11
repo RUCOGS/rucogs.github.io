854a3587a5aa61e0b3eeb9f4e752b37a5943b467
Atlinx <atlinuxtm@gmail.com>
2022-03-11 08:20:35 -0500

    Update FAQ styling

---
+++ src/app/components/question-panel/question-panel.component.ts
@@ -0,0 +1,19 @@
+import { Component, HostBinding, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-question-panel',
+  templateUrl: './question-panel.component.html',
+  styleUrls: ['./question-panel.component.css']
+})
+export class QuestionPanelComponent implements OnInit {
+
+  constructor() { }
+
+  @HostBinding('class.mat-expansion-panel') isExpansionPanel;
+  @Input() iconifyIcon: string = "";
+  @Input() title: string = "";
+  
+  ngOnInit(): void {
+  }
+
+}
