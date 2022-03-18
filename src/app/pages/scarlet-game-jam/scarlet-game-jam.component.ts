f8541ccdf18117bc844fbb5ff41cfe44514fc89d
Atlinx <atlinuxtm@gmail.com>
2022-03-18 19:32:18 -0400

    Prepped SGJ for final commit

--- src/app/pages/scarlet-game-jam/scarlet-game-jam.component.ts
+++ src/app/pages/scarlet-game-jam/scarlet-game-jam.component.ts
@@ -18,6 +18,14 @@ export class ScarletGameJamComponent implements OnInit {
   startDateCalendarEventLink: string = "https://calendar.google.com/calendar/render?action=TEMPLATE&dates=20211110T000000Z%2F20211110T020000Z&details=Meetup%20before%20Scarlet%20Game%20Jam%20to%20meet%20those%20participating%2C%20talk%20about%20your%20ideas%2C%20and%20get%20hyped%21&location=The%20CAVE%20-%20Hill%20250&text=Scarlet%20Game%20Jam%20Meetup%20and%20Discussion";
   endDateCalendarEventLink: string = "https://calendar.google.com/calendar/render?action=TEMPLATE&dates=20211120%2F20211121&details=Show%20off%20the%20awesome%20game%20you%20created%20and%20see%20other%20peoples%27%20as%20well%21&location=The%20CAVE%20-%20Hill%20250&text=Scarlet%20Game%20Jam%20Showcase%20and%20End";
 
+  merchLink: string = "http://scarlet-game-jam.allcolorsllc.com/";
+
+  startDateTime: string = "TBD";
+  endDateTime: string = "TBD";
+
+  startDateEventPage: string = ""//"https://rutgers.campuslabs.com/engage/event/7566450";
+  endDateEventPage: string = ""//"https://rutgers.campuslabs.com/engage/event/7566451";
+
   constructor(public breakpointManager: BreakpointManagerService, public settings: SettingsService) { }
 
   ngOnInit(): void {
