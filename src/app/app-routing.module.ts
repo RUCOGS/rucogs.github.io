d703bb947160599f66ea4d204d707fd8806fbb87
Atlinx <atlinuxtm@gmail.com>
2022-03-24 22:40:18 -0400

    Reset scroll on webpage switch

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CalendarComponent } from './pages/calendar/calendar.component';
+import { HomeComponent } from './pages/home/home.component';
+import { PicturesComponent } from './pages/pictures/pictures.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { ResourcesComponent } from './pages/resources/resources.component';
+import { ScarletGameJamComponent } from './pages/scarlet-game-jam/scarlet-game-jam.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'calendar', component: CalendarComponent },
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'pictures', component: PicturesComponent },
+  { path: 'resources', component: ResourcesComponent },
+  { path: 'scarlet-game-jam', component: ScarletGameJamComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' }
+]
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { scrollPositionRestoration: 'enabled' })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
