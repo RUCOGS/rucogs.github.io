824f640933622b3d0388311be673e58164308187
sorrer <sorrer@gmail.com>
2022-05-09 17:33:19 -0400

    President Farewell

--- src/app/utils/blog-page-articles.ts
+++ src/app/utils/blog-page-articles.ts
@@ -4,6 +4,15 @@ export const BlogPageArticlesDir = "assets/blog-page-articles/";
 
 export const BlogPageArticles = [
   new ArticleInfo(
+    /* File path   */ "2022/alexander-farewell",
+    /* Title       */ "Alexander Xie - Presidental Farewell",
+    /* Description */ "It's been a pleasure being the president of COGS.",
+    /* Date        */ "May 9, 2022",
+    /* Image Path  */ "assets/images/banner.png",
+    /* Authors     */ ["Alexander Xie"],
+    /* Tags        */ ["Club", "Article"],
+  ),
+  new ArticleInfo(
     /* File path   */ "2022/sgj-spring",
     /* Title       */ "Scarlet Game Jam S2022 Recap",
     /* Description */ "Recounting how scarlet game jam went.",
