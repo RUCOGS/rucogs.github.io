1a7f8e1c0db5fff17a29db623b3f29092bb0e6a1
Atlinx <atlinuxtm@gmail.com>
2022-06-23 02:23:38 -0400

    Update FAQ to dynamically display correct semester

--- src/app/pages/home/home/home-page.component.ts
+++ src/app/pages/home/home/home-page.component.ts
@@ -11,7 +11,26 @@ import { SettingsService } from '@src/_settings';
 })
 export class HomePageComponent implements OnInit {
 
-  constructor(public settings: SettingsService) { }
+  currentSemester: string = "";
+
+  constructor(public settings: SettingsService) {
+    var now = new Date();
+    var markerOne = new Date(`1/1/${now.getFullYear()}`);
+    var markerTwo = new Date(`5/15/${now.getFullYear()}`);
+    var markerThree = new Date(`12/15/${now.getFullYear()}`);
+    var markerFour = new Date(`12/31/${now.getFullYear()}`);
+
+    // one to two = Spring
+    // two to three = Fall
+    // three to four = Spring
+
+    if (markerOne <= now && now < markerTwo)
+      this.currentSemester = "Spring " + now.getFullYear();
+    else if (markerTwo <= now && now < markerThree)
+      this.currentSemester = "Fall " + now.getFullYear();
+    else if (markerThree <= now && now <= markerFour)
+      this.currentSemester = "Spring " + (now.getFullYear() + 1);
+  }
 
   ngOnInit(): void {
   }
