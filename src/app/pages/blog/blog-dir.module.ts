1571da51f4e1ce9504967c534197db7fe15c9e64
Atlinx <atlinuxtm@gmail.com>
2022-06-24 01:23:03 -0400

    Fix article-page routing

--- src/app/pages/blog/blog-dir.module.ts
+++ src/app/pages/blog/blog-dir.module.ts
@@ -1,10 +1,21 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { Route, RouterModule } from '@angular/router';
+import { Route, RouterModule, UrlSegment } from '@angular/router';
 
 const ROUTES: Route[] = [
   { path: '', loadChildren: () => import('./blog-page/blog-page.module').then(m => m.BlogPageModule) },
-  { path: '**', loadChildren: () => import('./article-page/article-page.module').then(m => m.ArticlePageModule) },
+  { matcher: (url: UrlSegment[]) => {
+      if (url.length > 1) {
+        return {
+          consumed: url,
+          posParams: {
+            article: new UrlSegment(url.join("/"), {})
+          }
+        };
+      }
+      return null;
+    }, loadChildren: () => import('./article-page/article-page.module').then(m => m.ArticlePageModule) 
+  },
 ];
 
 @NgModule({
