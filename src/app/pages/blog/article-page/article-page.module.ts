1189067e1ef2f8bee5c9a078ef6c81311150615e
Atlinx <atlinuxtm@gmail.com>
2022-06-24 01:17:30 -0400

    Rename articles module, update packages, update readme with debugging info

--- src/app/pages/blog/article-page/article-page.module.ts
+++ src/app/pages/blog/article-page/article-page.module.ts
@@ -1,27 +1,44 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ArticlePageComponent } from './article/article-page.component';
-import { TagsModule } from '@src/app/modules/tags/tags.module';
+import { NgModule } from '@angular/core';
+import { Route, RouterModule, UrlSegment } from '@angular/router';
 import { MarkdownNavModule } from '@src/app/modules/markdown-nav/markdown-nav.module';
-import { MarkdownModule } from 'ngx-markdown';
+import { TagsModule } from '@src/app/modules/tags/tags.module';
 import { CoreModule } from '@src/app/modules/_core/core.module';
+import { SettingsModule } from '@src/app/settings/_settings.module';
+import { MarkdownModule } from 'ngx-markdown';
+import { ArticlePageComponent } from './article/article-page.component';
 
 export { ArticlePageComponent };
 
+const ROUTES: Route[] = [
+  { 
+    matcher: (url: UrlSegment[]) => {
+      if (url.length > 1 && url[0].path === "blog") {
+        return {
+          consumed: url,
+          posParams: {
+            article: new UrlSegment(url.slice(1).join("/"), {})
+          }
+        };
+      }
+      return null;
+    }, 
+    component: ArticlePageComponent 
+  },
+];
 
 @NgModule({
   declarations: [
     ArticlePageComponent
   ],
   imports: [
+    RouterModule.forChild(ROUTES),
     CommonModule,
     TagsModule,
     MarkdownModule,
     MarkdownNavModule,
-    CoreModule
-  ],
-  exports: [
-    ArticlePageComponent
+    CoreModule,
+    SettingsModule
   ]
 })
 export class ArticlePageModule { }
