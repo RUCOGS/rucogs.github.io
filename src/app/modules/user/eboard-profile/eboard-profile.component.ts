30fbff47441ceec8ba5a55ead9bd5e4b5f971971
Atlinx <atlinuxtm@gmail.com>
2022-06-25 03:51:58 -0400

    Add current role display for eboard

--- src/app/modules/user/eboard-profile/eboard-profile.component.ts
+++ src/app/modules/user/eboard-profile/eboard-profile.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { CdnService } from '@src/app/services/cdn.service';
-import { EBoard } from '@src/generated/graphql-endpoint.types';
+import { EBoard, RoleCode } from '@src/generated/graphql-endpoint.types';
 import { PartialDeep } from 'type-fest';
 
 @Component({
@@ -17,4 +17,35 @@ export class EboardProfileComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  hasRoles() {
+    const currYear = new Date().getFullYear();
+    return this.eBoard.terms?.find(x => x?.year === currYear) !== undefined;
+  }
+
+  getRoles() {
+    const currYear = new Date().getFullYear();
+    if (this.eBoard.terms && this.eBoard.terms.length > 0)
+      return this.eBoard.terms.find(x => x?.year === currYear)?.roles?.map(x => x?.roleCode as RoleCode) ?? [];
+    return [];
+  }
+
+  getEBoardReign() {
+    const currYear = new Date().getFullYear();
+    if (this.eBoard.terms && this.eBoard.terms.length > 0) {
+      if (!this.eBoard.terms[0]?.year)
+        return "";
+      let minYear = this.eBoard.terms[0].year;
+      let maxYear = this.eBoard.terms[0].year;
+      for (const term of this.eBoard.terms) {
+        if (!term?.year)
+          continue;
+        if (term.year < minYear)
+          minYear = term.year;
+        if (term.year > maxYear)
+          maxYear = term.year;
+      }
+      return `${minYear} - ${maxYear}`;
+    }
+    return "";
+  }
 }
