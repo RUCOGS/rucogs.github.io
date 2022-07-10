04c253188c85b0cba884d525ef16f30d14dcb4df
Atlinx <atlinuxtm@gmail.com>
2022-07-10 05:51:09 -0400

    Add banner colors

--- src/app/modules/user/profile-overlay/profile-overlay.component.ts
+++ src/app/modules/user/profile-overlay/profile-overlay.component.ts
@@ -1,11 +1,23 @@
 import { animate, AnimationBuilder, AnimationMetadata, AnimationPlayer, style } from '@angular/animations';
 import { Overlay, OverlayRef } from '@angular/cdk/overlay';
 import { TemplatePortal } from '@angular/cdk/portal';
-import { Component, ElementRef, Input, OnDestroy, TemplateRef, ViewChild, ViewContainerRef } from '@angular/core';
+import {
+  AfterViewChecked,
+  Component,
+  ElementRef,
+  Input,
+  OnDestroy,
+  TemplateRef,
+  ViewChild,
+  ViewContainerRef,
+} from '@angular/core';
+import { Color } from '@src/app/classes/_classes.module';
 import { CdnService } from '@src/app/services/cdn.service';
 import { CssLengthService } from '@src/app/services/css-length.service';
 import { getClassYearString, getSidePanelPositions } from '@src/app/utils/_utils.module';
 import { User } from '@src/generated/graphql-endpoint.types';
+import { SettingsService } from '@src/_settings';
+import ColorThief from 'colorthief';
 import { PartialDeep } from 'type-fest';
 
 @Component({
@@ -13,19 +25,24 @@ import { PartialDeep } from 'type-fest';
   templateUrl: './profile-overlay.component.html',
   styleUrls: ['./profile-overlay.component.css'],
 })
-export class ProfileOverlayComponent implements OnDestroy {
+export class ProfileOverlayComponent implements AfterViewChecked, OnDestroy {
   @Input() user: PartialDeep<User> = {};
   @ViewChild('overlayTemplate', { read: TemplateRef }) overlayTemplate?: TemplateRef<any>;
   get overlayAnimRoot() {
     return this.overlayRef?.overlayElement.querySelector('.overlayAnimRoot');
   }
+  get avatar() {
+    return this.overlayRef?.overlayElement.querySelector<HTMLImageElement>('img.profile-overlay.avatar');
+  }
 
+  bannerColor: Color = Color.Types.white;
   hover: boolean = false;
   overlayRef?: OverlayRef;
   currAnimPlayer?: AnimationPlayer;
 
   constructor(
     public cdnService: CdnService,
+    public settings: SettingsService,
     private animBuilder: AnimationBuilder,
     private overlay: Overlay,
     private viewContainerRef: ViewContainerRef,
@@ -33,9 +50,35 @@ export class ProfileOverlayComponent implements OnDestroy {
     private cssLength: CssLengthService,
   ) {}
 
+  setupBannerColorListeners: boolean = false;
+  ngAfterViewChecked(): void {
+    this.trySetupBannerColorListeners();
+  }
+
   ngOnDestroy(): void {
     if (this.currAnimPlayer) this.currAnimPlayer.destroy();
     if (this.overlayRef) this.overlayRef.dispose();
+  }
+
+  trySetupBannerColorListeners() {
+    if (this.setupBannerColorListeners) return;
+
+    const img = this.avatar;
+    if (img) {
+      img.setAttribute('crossOrigin', '');
+      const colorThief = new ColorThief();
+      img.addEventListener('load', () => {
+        const [r, g, b] = colorThief.getColor(img);
+        this.bannerColor = new Color(r, g, b);
+      });
+      this.setupBannerColorListeners = true;
+    }
+  }
+
+  getBannerContainerStyle() {
+    return {
+      ...(this.bannerColor && { 'background-color': this.bannerColor.hexString() }),
+    };
   }
 
   readonly timings = `200ms ease-out`;
