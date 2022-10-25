1fef67c80a356fc37cbe7b00581696e96d63913f
Atlinx <atlinuxtm@gmail.com>
2022-10-25 02:40:50 -0400

    Add SGJ page WIP

--- src/app/pages/scarlet-game-jam/scarlet-game-jam/scarlet-game-jam-page.component.ts
+++ src/app/pages/scarlet-game-jam/scarlet-game-jam/scarlet-game-jam-page.component.ts
@@ -13,18 +13,18 @@ declare var FlipDown: any;
   },
 })
 export class ScarletGameJamPageComponent implements OnInit {
-  startDate: string = 'April 10';
-  endDate: string = 'April 17';
+  startDate: string = 'Nov 13';
+  endDate: string = 'Nov 20';
   startDateCalendarEventLink: string =
     'https://calendar.google.com/calendar/render?action=TEMPLATE&dates=20211110T000000Z%2F20211110T020000Z&details=Meetup%20before%20Scarlet%20Game%20Jam%20to%20meet%20those%20participating%2C%20talk%20about%20your%20ideas%2C%20and%20get%20hyped%21&location=The%20CAVE%20-%20Hill%20250&text=Scarlet%20Game%20Jam%20Meetup%20and%20Discussion';
   endDateCalendarEventLink: string =
     'https://calendar.google.com/calendar/render?action=TEMPLATE&dates=20211120%2F20211121&details=Show%20off%20the%20awesome%20game%20you%20created%20and%20see%20other%20peoples%27%20as%20well%21&location=The%20CAVE%20-%20Hill%20250&text=Scarlet%20Game%20Jam%20Showcase%20and%20End';
 
-  eventActive: boolean = true;
+  eventActive: boolean = false;
 
   merchLink: string = 'http://scarlet-game-jam.allcolorsllc.com/';
-  signupLink: string = 'https://forms.gle/jwcnZoYr3cAM4XHMA';
-  itchioLink: string = 'https://itch.io/jam/sgj2022s';
+  signupLink: string = '';
+  itchioLink: string = '';
 
   startDateTime: string = '2pm - 6pm';
   endDateTime: string = '11am - 11pm';
@@ -38,7 +38,8 @@ export class ScarletGameJamPageComponent implements OnInit {
 
   ngOnInit(): void {
     if (!this.eventActive) {
-      var finalDateSeconds = new Date(this.startDate + ', 2022 16:00:00 EST').getTime() / 1000;
+      var finalDateSeconds =
+        new Date(this.startDate + ', ' + new Date().getFullYear() + ' 16:00:00 EST').getTime() / 1000;
       var flipdown = new FlipDown(finalDateSeconds, 'sgj-countdown', {
         theme: 'dark',
       }).start();
