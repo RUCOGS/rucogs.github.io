89f533a65d7e14668d789fb82f6bef0d6e877c03
Atlinx <atlinuxtm@gmail.com>
2022-10-26 00:55:26 -0400

    Update SGJ page

--- src/app/pages/scarlet-game-jam/scarlet-game-jam/scarlet-game-jam-page.component.ts
+++ src/app/pages/scarlet-game-jam/scarlet-game-jam/scarlet-game-jam-page.component.ts
@@ -15,22 +15,18 @@ declare var FlipDown: any;
 export class ScarletGameJamPageComponent implements OnInit {
   startDate: string = 'Nov 13';
   endDate: string = 'Nov 20';
-  startDateCalendarEventLink: string =
-    'https://calendar.google.com/calendar/render?action=TEMPLATE&dates=20211110T000000Z%2F20211110T020000Z&details=Meetup%20before%20Scarlet%20Game%20Jam%20to%20meet%20those%20participating%2C%20talk%20about%20your%20ideas%2C%20and%20get%20hyped%21&location=The%20CAVE%20-%20Hill%20250&text=Scarlet%20Game%20Jam%20Meetup%20and%20Discussion';
-  endDateCalendarEventLink: string =
-    'https://calendar.google.com/calendar/render?action=TEMPLATE&dates=20211120%2F20211121&details=Show%20off%20the%20awesome%20game%20you%20created%20and%20see%20other%20peoples%27%20as%20well%21&location=The%20CAVE%20-%20Hill%20250&text=Scarlet%20Game%20Jam%20Showcase%20and%20End';
 
   eventActive: boolean = false;
 
   merchLink: string = 'http://scarlet-game-jam.allcolorsllc.com/';
   signupLink: string = 'https://forms.gle/3QT1gDhXqUGdvhhn6';
-  itchioLink: string = '';
+  itchioLink: string = 'https://itch.io/jam/scarlet-game-jam-fall-2022';
 
-  startDateTime: string = '2pm - 6pm';
+  startDateTime: string = '4pm - 6pm';
   endDateTime: string = '11am - 11pm';
 
-  startDateEventPage: string = 'https://rutgers.campuslabs.com/engage/event/8000723';
-  endDateEventPage: string = 'https://rutgers.campuslabs.com/engage/event/8000723';
+  startDateEventPage: string = 'https://rutgers.campuslabs.com/engage/event/8543685';
+  endDateEventPage: string = 'https://rutgers.campuslabs.com/engage/event/8543686';
 
   constructor(public breakpointManager: BreakpointManagerService, public settings: SettingsService) {}
 
