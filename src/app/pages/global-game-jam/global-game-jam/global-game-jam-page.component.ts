c33bfe4084a61ccc433e8ef1a251d91db1b051a1
atlinx <atlinuxtm@gmail.com>
2023-01-13 18:47:38 -0500

    Add GGJ

--- src/app/pages/global-game-jam/global-game-jam/global-game-jam-page.component.ts
+++ src/app/pages/global-game-jam/global-game-jam/global-game-jam-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewChecked, AfterViewInit, Component, OnInit } from '@angular/core';
 import { BreakpointManagerService } from '@app/services/breakpoint-manager.service';
 import { getSemesterString } from '@src/app/utils/duration-utils';
 import { SettingsService } from '@src/_settings';
@@ -12,30 +12,21 @@ declare var FlipDown: any;
     class: 'page',
   },
 })
-export class GlobalGameJamPageComponent implements OnInit {
-  startDate: string = 'Nov 13';
-  endDate: string = 'Nov 20';
+export class GlobalGameJamPageComponent implements AfterViewInit {
+  startDate: string = 'Feb 3 5:00 pm';
 
-  eventActive: boolean = true;
+  eventActive: boolean = false;
 
-  merchLink: string = 'http://global-game-jam.allcolorsllc.com/';
-  signupLink: string = 'https://forms.gle/3QT1gDhXqUGdvhhn6';
-  itchioLink: string = 'https://itch.io/jam/global-game-jam-fall-2022';
-
-  startDateTime: string = '4pm - 6pm';
-  endDateTime: string = '11am - 11pm';
-
-  startDateEventPage: string = 'https://rutgers.campuslabs.com/engage/event/8543685';
-  endDateEventPage: string = 'https://rutgers.campuslabs.com/engage/event/8543686';
+  siteLink: string = 'https://globalgamejam.org/2023/jam-sites/rutgers-new-brunswick-creation-games-society';
 
   constructor(public breakpointManager: BreakpointManagerService, public settings: SettingsService) {}
 
   getSemesterString = getSemesterString;
-
-  ngOnInit(): void {
+  
+  ngAfterViewInit(): void {
     if (!this.eventActive) {
       var finalDateSeconds =
-        new Date(this.startDate + ', ' + new Date().getFullYear() + ' 16:00:00 EST').getTime() / 1000;
+        new Date(this.startDate + ', ' + new Date().getFullYear() + ' EST').getTime() / 1000;
       var flipdown = new FlipDown(finalDateSeconds, 'sgj-countdown', {
         theme: 'dark',
       }).start();
