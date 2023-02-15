b5c11d8d1d05e0520adac55b7efd9b22d46eeaa4
Atlinx <atlinuxtm@gmail.com>
2023-02-15 00:31:28 -0500

    Fix eboard roles not showing due to year search failing

--- src/app/modules/eboard/eboard-profile/eboard-profile.component.ts
+++ src/app/modules/eboard/eboard-profile/eboard-profile.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { CdnService } from '@src/app/services/cdn.service';
+import { getCurrentAcademicYear } from '@src/app/utils/user-utils';
 import { EBoard, RoleCode } from '@src/generated/graphql-endpoint.types';
 import { PartialDeep } from 'type-fest';
 
@@ -16,23 +17,14 @@ export class EboardProfileComponent implements OnInit {
   ngOnInit(): void {}
 
   hasRoles() {
-    const now = new Date();
-    const semesterStart = new Date(`${now.getFullYear()}-09-1`);
-    let lookupYear;
-    if (now < semesterStart) {
-      // We're in the summer/spring semester, but we're not in fall yet, so we're still in the previous year
-      lookupYear = now.getFullYear() - 1;
-    } else {
-      // We're in the fall semester
-      lookupYear = now.getFullYear();
-    }
+    const lookupYear = getCurrentAcademicYear();
     return this.eBoard.terms?.find((x) => x?.year === lookupYear) !== undefined;
   }
 
   getRoles() {
-    const currYear = new Date().getFullYear();
+    const lookupYear = getCurrentAcademicYear();
     if (this.eBoard.terms && this.eBoard.terms.length > 0)
-      return this.eBoard.terms.find((x) => x?.year === currYear)?.roles?.map((x) => x?.roleCode as RoleCode) ?? [];
+      return this.eBoard.terms.find((x) => x?.year === lookupYear)?.roles?.map((x) => x?.roleCode as RoleCode) ?? [];
     return [];
   }
 
--- src/app/utils/user-utils.ts
+++ src/app/utils/user-utils.ts
@@ -1,3 +1,15 @@
+export function getCurrentAcademicYear() {
+  const now = new Date();
+  const semesterStart = new Date(`${now.getFullYear()}-09-1`);
+  if (now < semesterStart) {
+    // We're in the summer/spring semester, but we're not in fall yet, so we're still in the previous year
+    return now.getFullYear() - 1;
+  } else {
+    // We're in the fall semester
+    return now.getFullYear();
+  }
+}
+
 export function getClassYearString(classYear: number) {
   const now = new Date();
 
@@ -6,17 +18,9 @@ export function getClassYearString(classYear: number) {
     return '🎓 Alumni';
   }
 
-  let calcYear;
-  const semesterStart = new Date(`${now.getFullYear()}-09-1`);
-  if (now < semesterStart) {
-    // We're in the summer/spring semester, but we're not in fall yet, so we're still in the previous year
-    calcYear = now.getFullYear() - 1;
-  } else {
-    // We're in the fall semester
-    calcYear = now.getFullYear();
-  }
+  let academicYear = getCurrentAcademicYear();
 
-  const grade = classYear - calcYear;
+  const grade = classYear - academicYear;
   if (grade === 1) {
     return '🎆 Senior';
   } else if (grade === 2) {
