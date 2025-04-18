0a2827459bb8a2e49fe940db3a6a2f9df6ac84e3
Nihal-Pinto <nihalpinto05@gmail.com>
2025-04-18 03:27:05 -0400

    yet another quick fix

--- src/app/settings/blog-page-articles.ts
+++ src/app/settings/blog-page-articles.ts
@@ -80,7 +80,7 @@ export const BlogPageArticles = [
     /* Title       */ "Scarlet Game Jam Spring 2025",
     /* Description */ "Recounting how scarlet game jam went.",
     /* Date        */ "April 17, 2025",
-    /* Image Path  */ "assets/assets/images/sgj-logo-s2025.png",
+    /* Image Path  */ "assets/images/sgj-logo-s2025.png",
     /* Authors     */ ["Nihal Pinto"],
     /* Tags        */ ["SGJ", "event", "spring"],
   ),
