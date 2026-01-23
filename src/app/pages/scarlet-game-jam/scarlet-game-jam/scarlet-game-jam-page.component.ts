6ed14174f421fda9884e839c1a3fb8fc59bc57ca
Nihal-Pinto <nihalpinto05@gmail.com>
2026-01-22 23:38:19 -0500

    SGJ Spring 2026 initial page launch

--- src/app/pages/scarlet-game-jam/scarlet-game-jam/scarlet-game-jam-page.component.ts
+++ src/app/pages/scarlet-game-jam/scarlet-game-jam/scarlet-game-jam-page.component.ts
@@ -13,19 +13,19 @@ declare var FlipDown: any;
   },
 })
 export class ScarletGameJamPageComponent implements OnInit {
-  startDate: string = 'Sep 23';
-  endDate: string = 'Sep 27';
+  startDate: string = 'Feb 3';
+  endDate: string = 'Feb 7';
 
   startDateTime: string = '7pm - 9pm';
-  endDateTime: string = '2pm - 9pm';
+  endDateTime: string = '12pm - 9pm';
 
   merchLink: string = 'http://scarlet-game-jam.allcolorsllc.com/';
-  signupLink: string = 'https://rutgers.campuslabs.com/engage/event/11610504'; //done
-  itchioLink: string = 'https://itch.io/jam/scarlet-game-jam-fall-2025';
+  signupLink: string = 'https://rutgers.campuslabs.com/engage/event/12101377'; //done
+  itchioLink: string = 'https://itch.io/jam/scarlet-game-jam-spring-2026';
   stickerPinDesignLink: string = 'https://forms.gle/2Kp8yfb3eDQJDz1CA';
 
-  startDateEventPage: string = 'https://rutgers.campuslabs.com/engage/event/11610504';
-  endDateEventPage: string = 'https://rutgers.campuslabs.com/engage/event/11610645';
+  startDateEventPage: string = 'https://rutgers.campuslabs.com/engage/event/12101377';
+  endDateEventPage: string = 'https://rutgers.campuslabs.com/engage/event/12101378';
 
   eventActive: boolean = true;
 
